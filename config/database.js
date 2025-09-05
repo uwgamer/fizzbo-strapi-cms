05194f17bedfe41aab4f65ab0338c223e32025c0
uwgamer <lemieuxaccts@gmail.com>
2025-09-05 13:20:21 -0400

    Update to use new simple password without special characters

--- config/database.js
+++ config/database.js
@@ -33,7 +33,7 @@ module.exports = ({ env }) => {
     console.log('🏗️ This will be replaced by environment variables at runtime');
     
     // Use your actual Supabase connection for build - environment vars will override at runtime
-    const buildTimeUrl = 'postgresql://postgres.dsnoxaolyvopzyrjzukb:Wus7V$@cuYgv@!d@db.dsnoxaolyvopzyrjzukb.supabase.co:5432/postgres';
+    const buildTimeUrl = 'postgresql://postgres.dsnoxaolyvopzyrjzukb:Wus7VcuYgvd@db.dsnoxaolyvopzyrjzukb.supabase.co:5432/postgres';
     const config = parse(buildTimeUrl);
     
     return {
